4946b345bf8233cb4bafa2197fd66e2cd6be82e8
AdiraMartin <125848723+AdiraMartin@users.noreply.github.com>
2023-09-19 18:07:27 +0700

    lepel3.py

--- lepel3.py
+++ lepel3.py
@@ -3,18 +3,20 @@ import streamlit as st
 def main():
     st.title("Ayok bedoa")
     prayer1 = st.text_input("tulis doa pertama lu disini, jangan lupa enter")
+    prayer2 = st.text_input("ayok tulis lagi doa ke 2 nanti di aminin, ENTER")
+    prayer3 = st.text_input("1 lagi deh yu deh, ENTEERRRRR, BIAR AAMIN PALING KERASS")
+
     if prayer1:
             st.caption('aamiin Ya Allah')
             st.image(r'mail doa.jpg')
-        prayer2 = st.text_input("ayok tulis lagi doa ke 2 nanti di aminin, ENTER")
-        if prayer2:
-                st.caption('AAMIIN YA ALLAH')
-                st.image(r'spidermendoa.jpg')
+    
+    if prayer2:
+        st.caption('AAMIIN YA ALLAH')
+        st.image(r'spidermendoa.jpg')
             
-            prayer3 = st.text_input("1 lagi deh yu deh, ENTEERRRRR, BIAR AAMIN PALING KERASS")
-            if prayer3:
-                    st.caption('AAAAAMIIIIIIIN YA ALLAH🤲🤲😭😭😭')
-                    st.image(r'gettyimages-1134792776-612x612.jpg')
+    if prayer3:
+        st.caption('AAAAAMIIIIIIIN YA ALLAH🤲🤲😭😭😭')
+        st.image(r'gettyimages-1134792776-612x612.jpg')
 
     video_path = r'Egaa.avi'
 
