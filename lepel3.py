c37fe822916782e60c8df7a5d226c2e3e1a3d422
AdiraMartin <125848723+AdiraMartin@users.noreply.github.com>
2023-09-19 17:43:12 +0700

    lepel3.py

--- lepel3.py
+++ lepel3.py
@@ -7,18 +7,18 @@ def main():
             st.caption('aamiin Ya Allah')
             st.image(r'mail doa.jpg')
             
+        
+        prayer2 = st.text_input("ayok doa lagi tulis nanti di aminin")
+        if prayer2:
+                st.caption('AAMIIN YA ALLAH')
+                st.image(r'spidermendoa.jpg')
     
-    prayer2 = st.text_input("ayok doa lagi tulis nanti di aminin")
-    if prayer2:
-            st.caption('AAMIIN YA ALLAH')
-            st.image(r'spidermendoa.jpg')
+        prayer3 = st.text_input("1 lagi deh yu deh")
+        if prayer2:
+                st.caption('AAAAAMIIIIIIIN YA ALLAH🤲🤲😭😭😭')
+                st.image(r'gettyimages-1134792776-612x612.jpg')
 
-    prayer3 = st.text_input("1 lagi deh yu deh")
-    if prayer2:
-            st.caption('AAAAAMIIIIIIIN YA ALLAH🤲🤲😭😭😭')
-            st.image(r'gettyimages-1134792776-612x612.jpg')
-
-    video_path = r'C:\Users\user\STREAMLIT\egaa.264'
+    video_path = r'Egaa.avi'
 
     play_button = st.button("Play Video")
 
