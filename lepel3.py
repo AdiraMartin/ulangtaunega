360b1efaef375d774fa984b4b514e984e8a62308
AdiraMartin <125848723+AdiraMartin@users.noreply.github.com>
2023-09-19 18:00:04 +0700

    lepel3.py

--- lepel3.py
+++ lepel3.py
@@ -2,29 +2,30 @@ import streamlit as st
 
 def main():
     st.title("Ayok bedoa")
-    prayer1 = st.text_input("tulis doa lu disini")
-    if prayer1:
-            st.caption('aamiin Ya Allah')
-            st.image(r'mail doa.jpg')
-            
-        
-        prayer2 = st.text_input("ayok doa lagi tulis nanti di aminin")
-        if prayer2:
-                st.caption('AAMIIN YA ALLAH')
-                st.image(r'spidermendoa.jpg')
     
-        prayer3 = st.text_input("1 lagi deh yu deh")
-        if prayer2:
-                st.caption('AAAAAMIIIIIIIN YA ALLAH🤲🤲😭😭😭')
-                st.image(r'gettyimages-1134792776-612x612.jpg')
+    prayer1 = st.text_input("Tulis doa pertama kamu di sini")
+    prayer2 = st.text_input("Tulis doa kedua kamu di sini")
+    prayer3 = st.text_input("Tulis doa ketiga kamu di sini")
 
-    video_path = r'Egaa.avi'
+    if prayer1:
+        st.caption('Aamiin Ya Allah')
+        st.image('mail_doa.jpg')
+
+    if prayer2:
+        st.caption('AAMIIN YA ALLAH')
+        st.image('spidermendoa.jpg')
+
+    if prayer3:
+        st.caption('AAAAAMIIIIIIIN YA ALLAH🤲🤲😭😭😭')
+        st.image('gettyimages-1134792776-612x612.jpg')
+
+    video_path = 'Egaa.avi'
 
     play_button = st.button("Play Video")
 
     if play_button:
         st.write("Playing video...")
-    
+
     st.video(video_path)
 
 if __name__ == "__main__":
