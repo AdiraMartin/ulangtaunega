d8651a43945e2a86d31a4b12c035ad0a74d76e0f
AdiraMartin <125848723+AdiraMartin@users.noreply.github.com>
2023-09-19 18:03:27 +0700

    lepel3.py

--- lepel3.py
+++ lepel3.py
@@ -7,15 +7,13 @@ def main():
     prayer2 = st.text_input("Tulis doa kedua kamu di sini")
     prayer3 = st.text_input("Tulis doa ketiga kamu di sini")
 
-    if prayer1:
+    if prayer1 and not prayer2 and not prayer3:
         st.caption('Aamiin Ya Allah')
         st.image('mail_doa.jpg')
-
-    elif prayer2:
+    elif not prayer1 and prayer2 and not prayer3:
         st.caption('AAMIIN YA ALLAH')
         st.image('spidermendoa.jpg')
-
-    elif prayer3:
+    elif not prayer1 and not prayer2 and prayer3:
         st.caption('AAAAAMIIIIIIIN YA ALLAH🤲🤲😭😭😭')
         st.image('gettyimages-1134792776-612x612.jpg')
 
