604b2c795434e408a1c776065d93bc3b5b363fe4
AdiraMartin <125848723+AdiraMartin@users.noreply.github.com>
2025-08-17 17:35:42 +0700

    lepel3.py

--- lepel3.py
+++ lepel3.py
@@ -10,7 +10,6 @@ def main():
             st.write(f"aamiin Ya Allah **{prayer1}**")
             st.image("mail doa.jpg")
     
-
     # Prayer 2
     prayer2 = st.text_input("Ayok tulis lagi doa ke-2 nanti di aminin, ENTER")
     if prayer2:
@@ -18,7 +17,6 @@ def main():
             st.write(f"AAMIIN YA ALLAH **{prayer2}**")
             st.image("spidermendoa.jpg")
     
-
     # Prayer 3
     prayer3 = st.text_input("1 lagi deh yu deh, ENTEERRRRR, BIAR AAMIN PALING KERASS")
     if prayer3:
@@ -26,16 +24,24 @@ def main():
             st.write(f"AAAAAMIIIIIIIN YA ALLAH 🤲🤲😭😭😭: **{prayer3}**")
             st.image("gettyimages-1134792776-612x612.jpg")
 
-
     # Video
     video_path = "https://youtu.be/xyMGELBwt0w"
     play_button = st.button("Play Video")
 
     if play_button:
         st.write("Playing video...")
-        
-    st.video(video_path)
+        st.video(video_path)
 
+    # 🎉 Kondisi: semua doa keisi + video diputar
+    if prayer1 and prayer2 and prayer3 and play_button:
+        st.balloons()
+        st.write("---")
+        st.subheader("💌 Surat kecil buat Ega")
+        st.write("""
+        egaaa selamat ulang taun yaaa!!  
+        gue gasabar banget ngeliat lu mencapai cita cita lu satu per satuu,  
+        semoga tahun ini dimudahkan semuanyaaaa 🥳✨
+        """)
 
 if __name__ == "__main__":
     main()
