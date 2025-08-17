dacdb1b9087b82097c611fe040d442c574bb6d7d
AdiraMartin <125848723+AdiraMartin@users.noreply.github.com>
2025-08-17 17:28:58 +0700

    lepel2.py

--- lepel2.py
+++ lepel2.py
@@ -1,20 +1,20 @@
 import streamlit as st
 
 def buatlepel2():
-    st.write("Hei, aku tau kadang hidup bisa terasa berat. Yuk sebentar aja kita lihat hal-hal indah yang mungkin sering kita lupa 💖")
+    st.write("egaaa, gue tauuu kadang kadang hidup tuh bisa terasa berat. mau gaaaa sebentaar aja kita lihat hal-hal indah yang mungkin sering kita lupa 💖")
 
     # Pertanyaan 1
-    q1 = st.text_input("✨ Hal kecil apa yang bikin kamu tersenyum akhir-akhir ini?")
+    q1 = st.text_input("✨ Hal kecil apa yang bikin lu tersenyum akhir-akhir ini? cobak tulis yang beneerrrr")
     if q1:
         st.success("Indah banget... bahkan hal kecil seperti itu bisa jadi alasan buat bersyukur 🥰")
 
     # Pertanyaan 2
-    q2 = st.text_input("💫 Kalau inget orang yang peduli sama kamu, siapa yang pertama kali terlintas?")
+    q2 = st.text_input("💫 Kalau inget orang yang peduli sama lu, siapa yang pertama kali terlintas?")
     if q2:
-        st.success(f"{q2} pasti bersyukur banget punya kamu dalam hidupnya ✨")
+        st.success(f"daaammn {q2} pasti bersyukur banget punya kamu dalam hidupnya ✨")
 
     # Pertanyaan 3
-    q3 = st.text_input("🌸 Hal apa yang bikin kamu merasa damai?")
+    q3 = st.text_input("🌸 Hal apa yang bikin lu merasa kek damai ajaa gituu?")
     if q3:
         st.success("Damai itu berharga banget... itu tanda hati kamu masih hangat 💖")
 
@@ -24,13 +24,23 @@ def buatlepel2():
         st.write("---")
         st.subheader("💌 Surat kecil untukmu")
         st.write("""
-        Hari ini mungkin terasa berat, tapi aku percaya tahun ini akan bawa banyak hal indah buatmu.  
-        Kamu sudah melewati banyak hal, dan kamu tetap berdiri. Itu bukti kalau kamu luar biasa.  
+        gaaa, mungkin akhir akhir ini terasa berat, tapi guaah percaya tahun ini akan bawa banyak hal indah buat luu.  
+        lu tuh keren tau udah ngelewatin banyak hal, dan LU TETEP BERDIRI. Itu bukti kalau lu luar biasaballahuakbaaarrrr.  
 
-        Aku bersyukur banget punya kamu di hidupku.  
-        Semoga setiap kali kamu lupa, game kecil ini bisa jadi pengingat:  
+        guah bersyukur banget kenal ama laubrek.  
+        semoga kalau setiap kali lu lupa, game kecil ini bisa jadi pengingat:  
 
-        **kamu dicintai, kamu cukup, kamu berharga.** 💖  
+        ven when days feel heavy, never forget this:  
+        ✨ **You are worthy of love.**  
+        ✨ **You are enough.**  
+        ✨ **You are valuable.**  
+        
+        And remember...  
+        🌟 You will be wealthy.  
+        🌟 You will be successful.  
+        🌟 You will be happy.  
+        🌟 You will be at peace.  
+        Because you deserve all the beautiful things life has to offer 💖 💖  aseeeekkkkkkkk ah malu
         """)
 
 if __name__ == "__main__":
