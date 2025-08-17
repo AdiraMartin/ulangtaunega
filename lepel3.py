8504ecdfd7b70c71630f4f0d529ee67fa92a73c9
AdiraMartin <125848723+AdiraMartin@users.noreply.github.com>
2025-08-17 16:22:36 +0700

    lepel3.py

--- lepel3.py
+++ lepel3.py
@@ -1,32 +1,44 @@
 import streamlit as st
 
 def main():
-    st.title("Ayok bedoa")
-    prayer1 = st.text_input("tulis doa pertama lu disini, jangan lupa enter")
-    prayer2 = st.text_input("ayok tulis lagi doa ke 2 nanti di aminin, ENTER")
-    prayer3 = st.text_input("1 lagi deh yu deh, ENTEERRRRR, BIAR AAMIN PALING KERASS")
+    st.title("Ayok Bedoa")
 
+    # Prayer 1
+    prayer1 = st.text_input("Tulis doa pertama lu disini, jangan lupa enter")
     if prayer1:
-            st.caption('aamiin Ya Allah')
-            st.image(r'mail doa.jpg')
-    
+        with st.container():
+            st.write(f"Doa kamu: **{prayer1}**")
+            st.image("mail doa.jpg")
+            st.caption("aamiin Ya Allah")
+
+    # Prayer 2
+    prayer2 = st.text_input("Ayok tulis lagi doa ke-2 nanti di aminin, ENTER")
     if prayer2:
-        st.caption('AAMIIN YA ALLAH')
-        st.image(r'spidermendoa.jpg')
-            
+        with st.container():
+            st.write(f"Doa kamu: **{prayer2}**")
+            st.image("spidermendoa.jpg")
+            st.caption("AAMIIN YA ALLAH")
+
+    # Prayer 3
+    prayer3 = st.text_input("1 lagi deh yu deh, ENTEERRRRR, BIAR AAMIN PALING KERASS")
     if prayer3:
-        st.caption('AAAAAMIIIIIIIN YA ALLAH🤲🤲😭😭😭')
-        st.image(r'gettyimages-1134792776-612x612.jpg')
+        with st.container():
+            st.write(f"Doa kamu: **{prayer3}**")
+            st.image("gettyimages-1134792776-612x612.jpg")
+            st.caption("AAAAAMIIIIIIIN YA ALLAH 🤲🤲😭😭😭")
 
-    video_path = r'https://youtu.be/xyMGELBwt0w'
-
+    # Video
+    video_path = "https://youtu.be/xyMGELBwt0w"
     play_button = st.button("Play Video")
 
     if play_button:
         st.write("Playing video...")
-    
+
     st.video(video_path)
+
 
 if __name__ == "__main__":
     main()
+
+
 
